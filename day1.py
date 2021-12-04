22dd569da32c88d90087cc663af4474b8a706858
joroosen <jo.roosen@gmail.com>
2021-12-04 10:45:01 +0100

    day1.py and day1_input.txt

--- day1.py
+++ day1.py
@@ -1,1 +1,10 @@
-# temp
+with open('day1_input.txt') as f:
+    lines = f.readlines()
+depth = 999999
+increased = 0
+for line in lines:
+    if int(line.strip()) > depth:
+        increased += 1
+    depth = int(line.strip())
+
+print(increased)
