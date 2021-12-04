b4b1c943c3982793a7f05c501354489b905d6a42
joroosen <jo.roosen@gmail.com>
2021-12-04 13:40:31 +0100

    day2 - part2

--- day2.py
+++ day2.py
@@ -16,6 +16,23 @@ for a in lines:
 print("Day 2 Part 1 : " + str(horizontal*depth))
 
 
+horizontal = 0
+depth = 0
+aim = 0
+
+for a in lines:
+    if a.split(' ')[0] == "forward":
+        horizontal = horizontal + int(a.split(' ')[1])
+        depth = depth + (aim * int(a.split(' ')[1]))
+    elif a.split(' ')[0] == "down":
+        aim = aim + int(a.split(' ')[1])
+    elif a.split(' ')[0] == "up":
+        aim = aim - int(a.split(' ')[1])
+
+print(horizontal * depth)
+
+
+
 
 
 
